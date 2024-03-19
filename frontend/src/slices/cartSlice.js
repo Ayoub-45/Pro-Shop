044a36a609e7721b93211e7e8e986d4cfd49a00c
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-03-19 23:58:04 +0100

    add method to cart slice

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,10 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
-  : { cartItems: {} };
+  : { cartItems: [] };
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+      const existItem = state.cartItems.find((x) => x._id === item._id);
+      if (existItem) {
+        state.cartItems = state.cartItems.map((x) =>
+          x._id === existItem._id ? item : x
+        );
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+      //calculate item's price
+      state.itemPrice = state.cartItems.reduce((acc, item) => acc + item.price);
+      //Calculate tax price
+      //calculate total price
+    },
+  },
 });
 export default cartSlice.reducer;
