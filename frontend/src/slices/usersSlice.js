27963023890d0017b7f7eb0613f4a42fb3aa1286
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-03-30 09:38:30 +0100

    Frontend: Create auth and user slice

---
+++ frontend/src/slices/usersSlice.js
@@ -0,0 +1,13 @@
+import { USERS_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/auth`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+  }),
+});
+export const { useLoginMutation } = usersApiSlice;
