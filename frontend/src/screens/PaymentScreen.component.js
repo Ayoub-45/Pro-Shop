85dc6531085f8732545b507322c0acdc9bc99bf9
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-04-19 16:32:54 +0100

    Finish creating the ui

--- frontend/src/screens/PaymentScreen.component.js
+++ frontend/src/screens/PaymentScreen.component.js
@@ -2,14 +2,26 @@ import React, { useState, useEffect } from "react";
 import FormContainer from "../components/FormContainer.component";
 import CheckoutSteps from "../components/Checkout.component";
 import { Form, Button, Col } from "react-bootstrap";
+import { savePaymentMethod, saveShippingAddress } from "../slices/cartSlice";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 function PaymentScreen() {
   const [paymentMethod, setPaymentMethod] = useState("Paypal");
-
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!saveShippingAddress) navigate("/shipping");
+  }, [saveShippingAddress, navigate]);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(savePaymentMethod(paymentMethod));
+    navigate("/placeholder");
+  };
   return (
     <FormContainer>
       <CheckoutSteps step1 step2 step3 />
       <h1>payment Method</h1>
-      <Form>
+      <Form onSubmit={submitHandler}>
         <Form.Group>
           <Form.Label as="legend">Form</Form.Label>
           <Col>
